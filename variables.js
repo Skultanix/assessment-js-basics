d3622d5ce9bb768d92ed9ea0db1eed03eb736d22
Caleb Ricks <99217033+Skultanix@users.noreply.github.com>
2022-02-11 10:37:24 -0700

    Variables section completed.

--- variables.js
+++ variables.js
@@ -3,15 +3,21 @@
 
 //CODE HERE 
 
+var myName = `Caleb`
+
 //////////////////PROBLEM 2////////////////////
 /* Create a variable called 'faveNum' with a value that is your favorite number. */
 
 //CODE HERE
 
+var faveNum = 6
+
 //////////////////PROBLEM 3////////////////////
 /* Create a variable called 'lovesCode' and assign it a boolean value. */
 
 //CODE HERE
+
+var lovesCode = true
 
 //////////////////PROBLEM 4////////////////////
 /*
@@ -22,9 +28,19 @@
 
 //CODE HERE
 
+if(faveNum === 13) {
+  console.log(`lucky 13!`);
+} else {
+  console.log(`not lucky 13`);
+}
+
 //////////////////PROBLEM 5////////////////////
 /*
   Write a for loop that prints out your faveNum 5 times.
 */
 
 //CODE HERE
+
+for(squirtle = 0; squirtle < 5; squirtle++) {
+  console.log(faveNum);
+}
