6397e2157ed11aca9ebb1aa67f86799378de5876
Caleb Ricks <99217033+Skultanix@users.noreply.github.com>
2022-02-11 10:54:29 -0700

    Arrays section complete.

--- arrays.js
+++ arrays.js
@@ -5,10 +5,17 @@ const faveColors = ['red', 'green', 'black']
 
 //CODE HERE
 
+let colorCopy = faveColors.slice(0)
+// console.log(faveColors.slice(0))
+
+
 //////////////////PROBLEM 2////////////////////
 /* Add a fourth color to the end of the 'colorCopy' array using the push method. */
 
 //CODE HERE
+
+colorCopy.push('maroon')
+// console.log(colorCopy)
 
 //////////////////PROBLEM 3////////////////////
 /*
@@ -20,6 +27,8 @@ const numbers = [1, 2, 3, 4, 5]
 
 //CODE HERE
 
+let middleNums = numbers.slice(1,4);
+// console.log(middleNums);
 
 //////////////////PROBLEM 4////////////////////
 /* 
@@ -34,3 +43,14 @@ const numbers = [1, 2, 3, 4, 5]
 const bigOrSmallArray = [1, 99, 42, 69, 102, -10, 159, 352]
 
 // CODE HERE
+
+let answers = []
+
+for(charmander = 0; charmander < 8; charmander++) {
+  if(bigOrSmallArray[charmander] > 100) {
+    answers.push('big')
+  } else {
+    answers.push(`small`)
+  }
+}
+// console.log(answers);
